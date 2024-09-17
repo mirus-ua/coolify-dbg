07bb7846d84c3cc89fcbf72eb72baa2813368695
Mirus <mirusim@gmail.com>
2024-09-17 18:25:47 +0300

    pb client example

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,9 +1,27 @@
 import type { MetaFunction } from "@remix-run/node";
-
+import { useLoaderData } from "@remix-run/react";
+import { getPocketbase } from "~/api/pb.server";
 export const meta: MetaFunction = () => {
   return [{ title: "Foo", description: "bar" }];
 };
 
+export async function loader() {
+  const pb = getPocketbase();
+  const records = await pb.collection<any>("users").getFullList();
+
+  return records;
+}
+
 export default function HomePage() {
-  return <p>example page</p>;
+  const users = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <p>example page</p>
+      <ul>
+        {users.map((usr) => (
+          <li key={usr.id}>{usr.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
